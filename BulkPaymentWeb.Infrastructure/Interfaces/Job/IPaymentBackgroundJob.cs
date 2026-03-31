3dcff5ba637e967a57fc0f96ec212aa15fcf0cd0
uu <uu546@mail.ru>
2026-03-31 22:53:24 +0300

    Добавить интерфейсы обработчика

---
+++ BulkPaymentWeb.Infrastructure/Interfaces/Job/IPaymentBackgroundJob.cs
@@ -0,0 +1,15 @@
+﻿namespace BulkPaymentWeb.Infrastructure.Jobs
+{
+    /// <summary>
+    /// Интерфейс фонового обработчика реестра платежей.
+    /// </summary>
+    public interface IPaymentBackgroundJob
+    {
+        /// <summary>
+        /// Метод выполняет чтение, валидацию и сохранение платежей из файла Excel.
+        /// </summary>
+        /// <param name="registryId">Id реестра в базе данных.</param>
+        /// <param name="filePath">Путь к временно сохраненному файлу на диске.</param>
+        Task ProcessRegistryAsync(int registryId, string filePath);
+    }
+}
---
+++ BulkPaymentWeb.Infrastructure/Interfaces/PaymentRegistry/IPaymentRegistryService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace BulkPaymentWeb.Infrastructure.Interfaces.PaymentRegistry
+{
+    public interface IPaymentRegistryService
+    {
+        /// <summary>
+        /// Метод создает запись в БД и ставит задачу в очередь.
+        /// </summary>
+        Task<int> CreateAndSendEnqueueAsync(IFormFile file);
+    }
+}
