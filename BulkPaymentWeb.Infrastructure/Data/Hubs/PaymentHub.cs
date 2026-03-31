07f721a038ca293d3261ac9f778f1fd4a8e16163
uu <uu546@mail.ru>
2026-03-31 22:52:01 +0300

    Дообаивть хаб

---
+++ BulkPaymentWeb.Infrastructure/Data/Hubs/PaymentHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BulkPaymentWeb.Infrastructure.Data.Hubs
+{
+    /// <summary>
+    /// Класс хаба для передачи обновлений о статусе обработки платежных реестров.
+    /// </summary>
+    public class PaymentHub : Hub
+    {
+        /// <summary>
+        /// Метод подключает клиента к группе конкретного реестра. 
+        /// </summary>
+        /// <param name="registryId">Id реестра.</param>
+        public async Task JoinRegistryGroupAsync(int registryId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, registryId.ToString());
+        }
+    }
+}
