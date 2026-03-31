f08f07de88d5ffa68c068d84258568da0b64204d
uu <uu546@mail.ru>
2026-03-31 22:45:58 +0300

    Добавить сущности

---
+++ BulkPaymentWeb.Domain/Entities/PaymentItemEntity.cs
@@ -0,0 +1,68 @@
+﻿namespace BulkPaymentWeb.Domain.Entities
+{
+    /// <summary>
+    /// Класс платежа из реестра.
+    /// </summary>
+    public class PaymentItemEntity
+    {
+        /// <summary>
+        /// PK.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Навигационное свойство.
+        /// </summary>
+        public PaymentRegistryEntity? PaymentRegistryEntity { get; set; }
+
+        /// <summary>
+        /// FK Id реестра.
+        /// </summary>
+        public int RegistryId { get; set; }
+
+        /// <summary>
+        /// ИНН организации, которая платит.
+        /// </summary>
+        public string PayerInn { get; set; }
+
+        /// <summary>
+        /// Расчетный счет плательщика.
+        /// </summary>
+        public string PayerAccount { get; set; }
+
+        /// <summary>
+        /// ИНН организации, которая получает деньги.
+        /// </summary>
+        public string ReceiverInn { get; set; }
+
+        /// <summary>
+        /// Расчет счет получателя.
+        /// </summary>
+        public string ReceiverAccount { get; set; }
+
+        /// <summary>
+        /// Банковский Идентификационный Код банка получателя.
+        /// </summary>
+        public string ReceiverBik { get; set; }
+
+        /// <summary>
+        /// Сумма платежа.
+        /// </summary>
+        public decimal Amount { get; set; }
+
+        /// <summary>
+        /// Текстовое описание: за что переводятся деньги.
+        /// </summary>
+        public string Purpose { get; set; }
+
+        /// <summary>
+        /// Признак: прошел ли платеж валидацию.
+        /// </summary>
+        public bool IsValid { get; set; }
+
+        /// <summary>
+        /// Текст ошибки.
+        /// </summary>
+        public string? ValidationError { get; set; }
+    }
+}
---
+++ BulkPaymentWeb.Domain/Entities/PaymentRegistryEntity.cs
@@ -0,0 +1,33 @@
+﻿namespace BulkPaymentWeb.Domain.Entities
+{
+    /// <summary>
+    /// Класс реестра платежей.
+    /// </summary>
+    public class PaymentRegistryEntity
+    {
+        /// <summary>
+        /// PK.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Имя загружаемого файла реестра.
+        /// </summary>
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// Дата и время загрузки.
+        /// </summary>
+        public DateTime UploadedAt { get; set; }
+
+        /// <summary>
+        /// Текущее состояние обработки реестра.
+        /// </summary>
+        public string Status { get; set; }
+
+        /// <summary>
+        /// Список платежей, которые связанны с реестром.
+        /// </summary>
+        public List<PaymentItemEntity>? PaymentItemEntities { get; set; }
+    }
+}
